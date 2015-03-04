7ac9402080aebd707cbf8621e93d0822fa778694
Thomas Birke <quafzi@netextreme.de>
2015-03-04 21:39:28 +0100

    Allow sorting of menu items

--- public/js/modules/menu.js
+++ public/js/modules/menu.js
@@ -1,5 +1,26 @@
 'use strict';
 
+/**
+ * Menu items consist of an id and a name.
+ * Additionally, they may have an array of children.
+ * To manipulate sorting of children, items may specify a weight,
+ * otherwise, they'll be sorted alphabetically by name.
+ *
+ * example item:
+ *
+ * {
+ *   id: example,
+ *   name: "This is an example item",
+ *   children: [
+ *     {id: foo, name: foo},
+ *     {id: bar, name: bar},
+ *     {id: baz, name: baz, weight: 1},
+ *   ]
+ * }
+ *
+ * Order of children will be: bar, foo, baz.
+ */
+
 (function (dime, m) {
   var menu = {
     viewItem: function (item) {
@@ -9,8 +30,23 @@
             ? m("a[href='" + item.route + "']", {config: m.route}, item.name)
             : item.name
         ),
-        (item.children || []).map(menu.viewItem),
+        (item.children || []).sort(menu.sort).map(menu.viewItem),
       ]);
+    },
+    sort: function (a, b) {
+      var weightA = _.isNumber(a.weight) ? a.weight : 0;
+      var weightB = _.isNumber(b.weight) ? b.weight : 0;
+      if (weightA == weightB) {
+        weightA = a.name;
+        weightB = b.name;
+      }
+      if (weightA > weightB) {
+        return 1;
+      }
+      if (weightA < weightB) {
+        return -1;
+      }
+      return 0;
     },
     view: function (items) {
       return dime.menu.map(this.viewItem);
