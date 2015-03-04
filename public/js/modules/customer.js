c1cfcd1c47a63d49a17c34aea859f5b1069fdd64
Thomas Birke <quafzi@netextreme.de>
2015-03-04 21:19:16 +0100

    Allow adding and removing customers

--- public/js/modules/customer.js
+++ public/js/modules/customer.js
@@ -4,17 +4,40 @@
   var customer = {
     controller: function () {
       dime.store.get('customers').done(function(customers) {
-        m.redraw()
+        m.redraw();
       })
     },
-    update: function(id) {
+    getUrl: function (id) {
+      return dime.apiUrl + dime.schema.customers.url + '/'
+        + (_.isObject(id) ? id.id : id);
+    },
+    create: function() {
+      var item = {
+        name: document.getElementById("name").value,
+        alias: document.getElementById("alias").value,
+      };
+      dime.store.add('customers', item);
+      dime.store.get('customers').done(function(customers) {
+        m.redraw();
+      })
+    },
+    update: function (id) {
       var item = {
         id: id,
         name: document.getElementById("name-" + id).textContent,
         alias: document.getElementById("alias-" + id).textContent,
-        url: dime.apiUrl + dime.schema.customers.url + '/' + id
+        url: customer.getUrl(id)
       };
       dime.store.update('customers', item);
+    },
+    remove: function (item) {
+      var msg = "Do you really want to delete your customer '" + item.name + "'?";
+      if (confirm(msg)) {
+        dime.store.remove('customers', {url: customer.getUrl(item)})
+          .done(function() {
+            document.getElementById('customer-' + item.id).remove();
+          });
+      }
     },
     viewOne: function (item) {
       return m("dl#customer-" + item.id, [
@@ -29,11 +52,29 @@
           contenteditable: true,
           oninput: function() { customer.update(item.id) }
         }, item.alias),
+        m("input[type=submit].delete", {
+          onclick: function() { customer.remove(item) },
+          value: 'Delete'
+        })
       ]);
+    },
+    viewAddForm: function() {
+      return m("div#new-customer", [
+        m("label[for=name]", 'Name'),
+        m("input#name[type='text'][name='name']"),
+        m("label[for=alias]", 'Alias'),
+        m("input#alias[type='text'][name='alias']"),
+        m("input#add[type='button']", {
+          onclick: function () { customer.create() },
+          value: "Add"
+        }),
+      ])
     },
     view: function() {
       var customers = dime.store.findAll('customers') || []
-      return m("div", customers.map(customer.viewOne))
+      var list = customers.map(customer.viewOne);
+      list.push(customer.viewAddForm());
+      return m("div", list);
     }
   }
 
