99f308ce763925fd390cde8823776545716303af
Danilo Kuehn <dk@nogo-software.de>
2015-03-26 23:34:47 +0100

    style prompt

--- public/js/modules/prompt/prompt.js
+++ public/js/modules/prompt/prompt.js
@@ -4,30 +4,43 @@
 
   dime.modules.prompt = {
     controller: function () {
-      var scope = {};
+      var scope = {
+        help: false
+      };
 
       return scope;
     },
     view: function (scope) {
-      return [
-        m("input", {
-          placeholder: "Add an activity",
-          onchange: submit
-        }),
-        m("div.help", [
-          m("div.basics", 'Enter a description to start an activity.'),
-          m("ul", [
-            m("li.dates.duration", 'You may specify a duration by entering something like 1h 10m or 1:10.'),
-            m("li.dates.start", 'You may specify a start by entering something like 13:50-'),
-            m("li.dates.end", 'You may specify an end by entering something like -15:10'),
-            m("li.dates.start-and-end", 'You may specify start and end by entering something like 13:50-15:10'),
-            m("li.customer", 'Use "@" to specify a customer.'),
-            m("li.project", 'Use "/" to specify a project.'),
-            m("li.service", 'Use ":" to specify a service.'),
-            m("li.tags", 'Use "#" to add tags.'),
+      var showHelp = ".hide";
+      if (scope.help) {
+        showHelp = "";
+      }
+      
+      return m(".card", 
+        m(".card-main", 
+          m(".card-inner", [
+            m("input.form-control", {
+              placeholder: "Add an activity",
+              onfocus: function() { scope.help = true },
+              onblur: function() { scope.help = false },
+              onchange: submit
+            }),
+            m("div.form-help.form-help-msg" + showHelp, [
+              m("span.basics", 'Enter a description to start an activity.'),
+              m("ul", [
+                m("li.dates.duration", 'You may specify a duration by entering something like 1h 10m or 1:10.'),
+                m("li.dates.start", 'You may specify a start by entering something like 13:50-'),
+                m("li.dates.end", 'You may specify an end by entering something like -15:10'),
+                m("li.dates.start-and-end", 'You may specify start and end by entering something like 13:50-15:10'),
+                m("li.customer", 'Use "@" to specify a customer.'),
+                m("li.project", 'Use "/" to specify a project.'),
+                m("li.service", 'Use ":" to specify a service.'),
+                m("li.tags", 'Use "#" to add tags.'),
+              ])
+            ])
           ])
-        ])
-      ];
+        )
+      );
     }
   };
 
