a2db08b51d99809127ead3a5e5010c2d9e7a0c83
Danilo Kuehn <dk@nogo-software.de>
2015-04-05 16:06:39 +0200

    add function fetch

--- public/js/vendor/resource.js
+++ public/js/vendor/resource.js
@@ -10,105 +10,125 @@
  * @returns {Resource}
  */
 var Resource = function (options) {
-    var that = this;
+  var that = this;
 
-    this.config = _.extend({
-        idAttribute: 'id',
-        sorted: false,
-        empty: {}
-    }, options);
-    this.collection = [];
-
-    m.request({ method: 'GET', url: this.config.url })
-    .then(function (list) {
-        that.collection = _.isFunction(that.config.sort) ? list.sort(that.config.sort) : list;
-        if (_.isFunction(that.config.model)) {
-          that.collection.forEach(function(item, idx, collection) {
-            collection[idx] = new that.config.model(item);
-          });
-        }
-        return that.collection;
-    });
+  this.collection = [];
+  this.config = _.extend({
+    idAttribute: 'id',
+    sorted: false,
+    empty: {}
+  }, options);
 };
 
+/**
+ * Return the defined empty object
+ * @param Object data
+ * @returns Object
+ */
 Resource.prototype.empty = function (data) {
-    data = data || {};
-    return _.extend({}, this.config.empty, data);
+  return _.extend({}, this.config.empty, data || {});
 };
 
 Resource.prototype.first = function () {
-    return (this.collection.length > 0) ? this.collection[0] : undefined;
+  return (this.collection.length > 0) ? this.collection[0] : undefined;
 };
 
 Resource.prototype.last = function () {
-    return (this.collection.length > 0) ? this.collection[this.collection.length - 1] : undefined;
+  return (this.collection.length > 0) ? this.collection[this.collection.length - 1] : undefined;
 };
 
 Resource.prototype.find = function (filter) {
-    if (_.isNumber(filter)) {
-        filter = {};
-        fiter[this.idAttribute] = filter;
-    }
-    return _.findWhere(this.collection, filter);
+  if (_.isNumber(filter)) {
+    filter = {};
+    filter[this.idAttribute] = filter;
+  }
+  return _.findWhere(this.collection, filter);
 };
 
 Resource.prototype.findAll = function () {
-    return this.collection;
+  return this.collection;
+};
+
+Resource.prototype.fetch = function () {
+  var that = this,
+      deferred = m.deferred();
+
+  m
+  .request({ method: 'GET', url: this.config.url })
+  .then(function (list) {
+    var c = _.isFunction(that.config.sort) ? list.sort(that.config.sort) : list;
+//    that.collection = _.isFunction(that.config.sort) ? list.sort(that.config.sort) : list;
+    if (_.isFunction(that.config.model)) {
+      c.forEach(function (item, idx, collection) {
+        collection[idx] = new that.config.model(item);
+      });
+    }
+
+    that.collection = c;
+    deferred.resolve(that.collection);
+    return that.collection;
+  });
+
+  deferred.promise(this.collection);
+  return deferred.promise;
 };
 
 Resource.prototype.persist = function (data) {
-    var url = this.config.url,
-        method = 'POST',
-        that = this,
-        deferred = m.deferred();
+  var url = this.config.url,
+      method = 'POST',
+      that = this,
+      deferred = m.deferred();
 
-    if (data[this.config.idAttribute]) {
-        url = this.config.url + '/' + data[this.config.idAttribute];
-        method = 'PUT';
+  if (data[this.config.idAttribute]) {
+    url = this.config.url + '/' + data[this.config.idAttribute];
+    method = 'PUT';
+  }
+
+  m
+  .request({ method: method, url: url, data: data })
+  .then(function (response) {
+    if (method === 'POST') { // create new
+      if (_.isFunction(that.config.model)) {
+        response = new that.config.model(response);
+      }
+      that.collection.push(response);
+      if (_.isFunction(that.config.sort)) {
+        that.collection = that.collection.sort(that.config.sort);
+      }
     }
+    deferred.resolve(response);
+    return response;
+  });
 
-    m.request({ method: method, url: url, data: data })
-    .then(function(response) {
-        if (method === 'POST') { // create new
-            if (_.isFunction(that.config.model)) {
-                response = new that.config.model(response);
-            }
-            that.collection.push(response);
-            if (_.isFunction(that.config.sort)) {
-                that.collection = that.collection.sort(that.config.sort);
-            }
-        }
-        deferred.resolve(response);
-        return response;
-    });
-    deferred.promise(data);
-    return deferred.promise;
+  deferred.promise(data);
+  return deferred.promise;
 };
 
 Resource.prototype.remove = function (data) {
-    var url = this.config.url,
-        method = 'DELETE',
-        that = this,
-        deferred = m.deferred(),
-        id = data[this.config.idAttribute];
+  var url = this.config.url,
+      method = 'DELETE',
+      that = this,
+      deferred = m.deferred(),
+      id = data[this.config.idAttribute];
 
-    if (id) {
-        url = this.config.url + '/' + id;
+  if (id) {
+    url = this.config.url + '/' + id;
 
-        m.request({ method: method, url: url })
-        .then(function(response) {
-            var idx = that.collection.indexOf(data),
-                item = data;
-            if (-1 < idx) {
-                item = that.collection.splice(idx, 1);
-            }
-            deferred.resolve(item);
-            return item;
+    m
+    .request({method: method, url: url})
+    .then(function (response) {
+      var idx = that.collection.indexOf(data),
+              item = data;
+      if (-1 < idx) {
+        item = that.collection.splice(idx, 1);
+      }
+      deferred.resolve(item);
+      return item;
 
-        });
-        deferred.promise({});
-    } else {
-        deferred.reject();
-    }
-    return deferred.promise;
+    });
+    deferred.promise({});
+  } else {
+    deferred.reject();
+  }
+  return deferred.promise;
 };
