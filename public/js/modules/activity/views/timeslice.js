eab05366f09286c465c499d3641a012f1c84458e
Thomas Birke <quafzi@netextreme.de>
2015-04-13 17:06:10 +0200

    Ref #6: Emit events in activity timeslice table view

--- public/js/modules/activity/views/timeslice.js
+++ public/js/modules/activity/views/timeslice.js
@@ -4,7 +4,7 @@
   dime.modules.activity.views.timesliceItem = function (item) {
     item = dime.model.Timeslice(item);
     var activity = this;
-    return m("tr#timeslice-" + item.id, [
+    var rowView = m("tr#timeslice-" + item.id, [
       m("td.start", moment(item.startedAt).format("DD.MM.YYYY HH:mm:ss")),
       m("td.stop", item.stoppedAt ? moment(item.stoppedAt).format("DD.MM.YYYY HH:mm:ss") : ""),
       m("td.duration#timeslice-duration-" + item.id, dime.helper.duration.format(item.calcDuration(), "seconds")),
@@ -12,20 +12,25 @@
         m("a.btn.btn-flat[href=#]", { onclick: function() { activity.removeTimeslice(item); return false; } }, m("span.icon.icon-delete"))
       ])
     ]);
+    dime.events.emit('activity-timeslice-table-row-view-after', {view: rowView, item: item, activity: activity});
+    return rowView;
   };
 
   dime.modules.activity.views.timeslices = function (current) {
     var items = current.timeslices || [];
 
+    var tableHead = m("thead",
+      m("tr", [
+        m("th", "Start"),
+        m("th", "End"),
+        m("th", "Duration"),
+        m("th.right-align", m("a.btn.btn-flat[href=#]", m("span.icon.icon-add")))
+      ])
+    );
+    dime.events.emit('activity-timeslice-table-head-view-after', {view: tableHead, activity: current});
+
     return m("table.bordered.responsive-table", [
-      m("thead",
-        m("tr", [
-          m("th", "Start"),
-          m("th", "End"),
-          m("th", "Duration"),
-          m("th.right-align", m("a.btn.btn-flat[href=#]", m("span.icon.icon-add")))
-        ])
-      ),
+      tableHead,
       m("tbody", items.map(dime.modules.activity.views.timesliceItem, current))
     ]);
   };
