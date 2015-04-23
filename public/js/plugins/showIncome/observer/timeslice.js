f95c8cb8478caf1cb087932211790dc7f7550bf0
Thomas Birke <quafzi@netextreme.de>
2015-04-23 17:21:25 +0200

    Call t() instead of outdated dime.translate in showIncome plugin

--- public/js/plugins/showIncome/observer/timeslice.js
+++ public/js/plugins/showIncome/observer/timeslice.js
@@ -1,12 +1,12 @@
 'use strict';
-(function (dime, m, _) {
+(function (dime, m, _, t) {
 
   dime.events.on('activity-timeslice-table-head-view-after', function(context) {
     if (false == dime.modules.setting.get('activity', 'display/showIncome', false)) {
       return;
     }
     context.view.children[0].children[4] = context.view.children[0].children[3];
-    context.view.children[0].children[3] = m('th.text-right', dime.translate('Income'));
+    context.view.children[0].children[3] = m('th.text-right', t('Income'));
   });
 
   dime.events.on('activity-timeslice-table-row-view-after', function(context) {
@@ -19,4 +19,4 @@
     context.view.children[3] = m('td.text-right', dime.helper.number.formatCurrency(income, '{number} €'));
   });
 
-})(dime, m, _);
+})(dime, m, _, t);
