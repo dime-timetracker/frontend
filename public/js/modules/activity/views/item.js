696c79df26118bd18e28d3f6b361c8d346347b3e
Danilo Kuehn <dk@nogo-software.de>
2015-05-28 17:35:01 +0200

    performance improvement - remove timesliceView from content array if not visible

--- public/js/modules/activity/views/item.js
+++ public/js/modules/activity/views/item.js
@@ -1,6 +1,6 @@
-'use strict';
-(function (dime, m, moment, _) {
-
+;(function (dime, m, moment, _) {
+  'use strict';
+  
   dime.timer = setInterval(m.redraw, 1000);
 
   var startStopButton = function(current) {
@@ -19,15 +19,13 @@
       " ",
       dime.helper.format.duration(current.totalDuration(), 'seconds')
     ]);
-  }
+  };
 
   dime.modules.activity.views.item = function (current, idx) {
-    var className = current.showTimeslices ? '' : '.hide';
-
     var badges = [
       dime.core.views.badge('customer', current),
       dime.core.views.badge('project', current),
-      dime.core.views.badge('service', current),
+      dime.core.views.badge('service', current)
     ];
 
     var badgesView = m('ul.nav.nav-list.badge-list.badge-list-small', badges);
@@ -61,11 +59,7 @@
     }, current.description);
     dime.events.emit('activity-item-actions-view-after', {view: descriptionsView, item: current});
 
-    var tagBadgesView = dime.modules.tag.views.input(current)
-
-    var timeslicesView = dime.modules.activity.views.timeslices(current);
-    dime.events.emit('activity-item-timeslices-view-after', {view: timeslicesView, item: current});
-
+    var tagBadgesView = dime.modules.tag.views.input(current);
 
     var tileClasses = ['tile'];
     var htmlId = '';
@@ -78,15 +72,20 @@
       htmlId = '#currentActivity';
     }
 
-    var result = m(htmlId + '.' + tileClasses.join('.'), [
+    var content = [
       m('.pull-left.tile-side', badgesView),
       m('.tile-action.tile-action-show', actionsView),
-      m('.tile-inner', [descriptionsView, tagBadgesView]),
-      m('.tile-sub' + className, timeslicesView)
-    ]);
+      m('.tile-inner', [descriptionsView, tagBadgesView])
+    ];
+    
+    if (current.showTimeslices) {
+      var timeslicesView = dime.modules.activity.views.timeslices(current);
+      dime.events.emit('activity-item-timeslices-view-after', {view: timeslicesView, item: current});
+      content.push(m('.tile-sub', timeslicesView));
+    }
 
+    var result = m(htmlId + '.' + tileClasses.join('.'), content);
     dime.events.emit('activity-item-view-after', {view: result, item: current});
-
     return result;
   };
 
