aede5ee3a75eb0ba7bd01290ea2bbb3594ca7068
Thomas Birke <quafzi@netextreme.de>
2015-07-03 09:02:02 +0200

    Refactor times parser

--- src/js/core/parser/times.js
+++ src/js/core/parser/times.js
@@ -5,87 +5,111 @@
 var moment = require('moment');
 var isEmpty = require('lodash/lang/isEmpty');
 
-module.exports = function (obj) {
-  var start = null;
-  var stop = null;
-
+function parseStartStop (obj) {
   // 01:23-03:49 (start and stop given)
   var regex = /(\d+:\d{2})-(\d+:\d{2})/;
   var matches = obj._text.match(regex);
-  if (null != matches) {
-    start = moment(moment().format('YYYY-MM-DD ') + matches[1]);
-    stop  = moment(moment().format('YYYY-MM-DD ') + matches[2]);
-    while (stop.isBefore(start)) {
-      start = start.subtract(1, 'day');
+  if (null !== matches) {
+    obj._start = moment(moment().format('YYYY-MM-DD ') + matches[1]);
+    obj._stop  = moment(moment().format('YYYY-MM-DD ') + matches[2]);
+    while (obj._stop.isBefore(obj._start)) {
+      obj._start = obj._start.subtract(1, 'day');
     }
+    return regex;
+  }
+}
+
+function parseStopOnly (obj) {
+  // -01:23 (only stop time given, starting now)
+  var regex = /(-(\d+):(\d{2})h?)/;
+  var matches = obj._text.match(regex);
+  if (null !== matches) {
+    obj._start = moment();
+    obj._stop = moment();
+    obj._stop.hours(matches[2]);
+    obj._stop.minutes(matches[3]);
+    while (obj._stop.isBefore(obj._start)) {
+      obj._stop = obj._stop.add(1, 'day');
+    }
+    return regex;
+  }
+}
+
+function parseStartOnly (obj) {
+  // 01:23- (only start time given, stopping now)
+  var regex = /((\d+):(\d{2})h?-)/;
+  var matches = obj._text.match(regex);
+  if (null != matches) {
+    obj._stop = moment();
+    obj._start = moment().hours(matches[2])
+      .minutes(matches[3]);
+    while (obj._stop.isBefore(obj._start)) {
+      obj._start = obj._start.subtract(1, 'day');
+    }
+    return regex;
+  }
+}
+
+function parseDuration (obj) {
+  // duration given, stopping now
+  var hours = 0;
+  var minutes = 0;
+  // 01:06 || 01:06h
+  var regex = /((\d+):(\d{2})h?)/;
+  var matches = obj._text.match(regex);
+  if (null !== matches) {
+    hours = matches[2];
+    minutes = matches[3];
   } else {
-    // -01:23 (only stop time given, starting now)
-    regex = /(-(\d+):(\d{2})h?)/;
+    // 01h 06m
+    regex = /(^| )((\d+)h( ?(\d+)m)?)/;
     matches = obj._text.match(regex);
-    if (null != matches) {
-      start = moment();
-      stop = moment();
-      stop.hours(matches[2]);
-      stop.minutes(matches[3]);
-      while (stop.isBefore(start)) {
-        stop = stop.add(1, 'day');
-      }
+    if (null !== matches) {
+      hours = matches[3];
+      minutes = matches[5];
     } else {
-      // 01:23- (only start time given, stopping now)
-      regex = /((\d+):(\d{2})h?-)/;
+      // 1,1 || 1,1h || 1.1 || 1.1h
+      regex = /(^| )(\d+)([\.,](\d+))?h/;
       matches = obj._text.match(regex);
-      if (null != matches) {
-        stop = moment();
-        start = moment().hours(matches[2])
-          .minutes(matches[3]);
-        while (stop.isBefore(start)) {
-          start = start.subtract(1, 'day');
-        }
-      } else {
-        // 01:23 (duration given, stopping now)
-        var hours = 0;
-        var minutes = 0;
-        regex = /((\d+):(\d{2})h?)/;
-        matches = obj._text.match(regex);
-        if (null != matches) {
-            hours = matches[2];
-            minutes = matches[3];
-        } else {
-          regex = /(^| )((\d+)h( ?(\d+)m)?)/;
-          matches = obj._text.match(regex);
-          if (null != matches) {
-            hours = matches[3];
-            minutes = matches[5];
-          } else {
-            regex = /(^| )(\d+)([\.,](\d+))?h/;
-            matches = obj._text.match(regex);
-            if (null == matches) {
-              return obj;
-            }
-            hours = matches[2];
-            minutes = matches[4]/10 * 60;
-          }
-        }
-        start = moment();
-        start.subtract(hours, 'hours');
-        start.subtract(minutes, 'minutes');
-        stop  = moment();
+      if (null !== matches) {
+        hours = matches[2];
+        minutes = matches[4]/10 * 60;
       }
     }
   }
+  if (0 < hours || 0 < minutes) {
+    obj._start = moment();
+    obj._start.subtract(hours, 'hours');
+    obj._start.subtract(minutes, 'minutes');
+    obj._stop = moment();
+    return regex;
+  }
+}
+
+module.exports = function (obj) {
+  obj._start = null;
+  obj._stop = null;
+
+  var matchedRegex = parseStartStop(obj) ||
+    parseStopOnly(obj) ||
+    parseStartOnly(obj) ||
+    parseDuration(obj);
 
   var timeslice = {};
-  if (start !== null) {
-    timeslice.startedAt = moment(start).format('YYYY-MM-DD HH:mm');
+  if (obj._start !== null) {
+    timeslice.startedAt = moment(obj._start).format('YYYY-MM-DD HH:mm');
   }
-  if (stop !== null) {
-    timeslice.stoppedAt = moment(stop).format('YYYY-MM-DD HH:mm');
+  if (obj._stop !== null) {
+    timeslice.stoppedAt = moment(obj._stop).format('YYYY-MM-DD HH:mm');
   }
+
   if (!isEmpty(timeslice)) {
     obj.timeslices = [ timeslice ];
   }
 
   // cut off parsed time
-  obj._text = obj._text.replace(regex, '');
+  delete obj._start;
+  delete obj._stop;
+  obj._text = obj._text.replace(matchedRegex, '');
   return obj;
 };
