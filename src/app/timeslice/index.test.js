5a48f0d5f5d0a3fcea748a6c25888b4a05540ecc
Thomas Birke <quafzi@netextreme.de>
2016-08-31 11:54:22 +0200

    Switch from camelCase stoppedAt to stopped_at in timeslice test

--- src/app/timeslice/index.test.js
+++ src/app/timeslice/index.test.js
@@ -9,7 +9,7 @@ describe('timeslice', () => {
 
   beforeEach(() => {
     item = {
-      startedAt: '2015-06-22 14:23:35'
+      started_at: '2015-06-22 14:23:35'
     }
   })
 
@@ -18,20 +18,20 @@ describe('timeslice', () => {
   })
 
   it('should be running', () => {
-    item.stoppedAt = '2015-06-22 16:43:11'
+    item.stopped_at = '2015-06-22 16:43:11'
     expect(timeslice.running(item)).to.be(false)
   })
 
   describe('should calculate timeslice durations', () => {
     it('of stopped timeslice', () => {
-      item.stoppedAt = '2015-06-22 16:43:10'
+      item.stopped_at = '2015-06-22 16:43:10'
       expect(timeslice.duration(item)).to.be(
         2 * 60 * 60 + 20 * 60 - 25
       )
     })
 
     it('of running timeslice', () => {
-      item.stoppedAt = undefined
+      item.stopped_at = undefined
       timeslice.now(moment('2015-06-22 18:43:10'))
       expect(timeslice.duration(item)).to.be(
         4 * 60 * 60 + 20 * 60 - 25
