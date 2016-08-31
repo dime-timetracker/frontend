1a39b999f52245f845f5f5b405f14b887687593b
Thomas Birke <quafzi@netextreme.de>
2016-08-31 14:34:16 +0200

    Fix currency tests that only failed in node du to missing locale data

--- src/lib/helper/currency.test.js
+++ src/lib/helper/currency.test.js
@@ -4,24 +4,26 @@ const expect = require('expect.js')
 const m = require('src/lib/mithril')
 require('intl')
 
-global.window = m.deps({
+const fakeWindow = {
   localStorage: {},
   dimeDebug: () => {},
   document: global.document,
   navigator: { language: 'de-DE' }
-})
+}
+global.window = m.deps(fakeWindow)
 
 const currency = require('./currency')
 
 describe('currency', () => {
   const nbsp = '\u00A0'
 
-  it('should convert number (3.22) into formated string € 3,22, if locale and currency are given', () => {
+  beforeEach(() => { global.window = m.deps(fakeWindow) })
+  it('should convert number (3.22) into formated string 3,22 €, if locale and currency are given', () => {
     let result = currency(3.22, { currency: 'EUR' }, 'de-DE')
     expect(result).to.be('3,22' + nbsp + '€')
   })
 
-  it('should convert number (3.22) into formated string € 3,22, if locale is given', () => {
+  it('should convert number (3.22) into formated string 3,22 €, if locale is given', () => {
     let result = currency(3.22, {}, 'de-DE')
     expect(result).to.be('3,22' + nbsp + '€')
 
@@ -29,7 +31,7 @@ describe('currency', () => {
     expect(result).to.be('3,22' + nbsp + '€')
   })
 
-  it('should convert number (3.22) into formated string € 3,22, using browser locale', () => {
+  it('should convert number (3.22) into formated string 3,22 €, using browser locale', () => {
     let result = currency(3.22)
     expect(result).to.be('3,22' + nbsp + '€')
   })
