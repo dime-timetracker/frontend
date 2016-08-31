d720ac21353e6a5d255d48a5480300e71a05a081
Thomas Birke <quafzi@netextreme.de>
2016-08-31 12:04:17 +0200

    (minor) apply standard js to store lib

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,36 +1,34 @@
-'use strict';
+'use strict'
 
 var Store = function () {
   if (!(this instanceof Store)) {
-    return new Store();
+    return new Store()
   }
-};
+}
 
-Store.prototype = Object.create(null);
-Store.prototype.constructor = Store;
+Store.prototype = Object.create(null)
+Store.prototype.constructor = Store
 
 Store.prototype.getItem = function (key) {
-  return (this[key] !== undefined) ? this[key] : undefined;
-};
+  return (this[key] !== undefined) ? this[key] : undefined
+}
 
 Store.prototype.setItem = function (key, value) {
-  this[key] = value;
-};
+  this[key] = value
+}
 
 Store.prototype.removeItem = function (key) {
   if (this[key] !== undefined) {
-    delete this[key];
+    delete this[key]
   }
-};
+}
 
 Store.prototype.clear = function (key) {
   for (var name in this) {
     if (this.hasOwnProperty(name)) {
-      delete this[name];
+      delete this[name]
     }
   }
-};
+}
 
-module.exports = global.window.localStorage || new Store();
-
-
+module.exports = global.window.localStorage || new Store()
