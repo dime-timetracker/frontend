046ca961e1bf28d42ad2694a8956a7eb2e70aa25
Thomas Birke <quafzi@netextreme.de>
2016-12-02 23:27:43 +0100

    Add missing invoice footer configs

--- src/app/report/settings.js
+++ src/app/report/settings.js
@@ -3,5 +3,8 @@
 module.exports = {
   'report.precision': { type: 'number', value: '900' },
   'report.invoice.sender': { type: 'textarea', rows: 5 },
-  'report.invoice.taxRate': { type: 'number', value: 19 }
+  'report.invoice.taxRate': { type: 'number', value: 19 },
+  'report.invoice.footer.companyName': { type: 'text', value: '' },
+  'report.invoice.footer.iban': { type: 'text', value: '' },
+  'report.invoice.footer.taxId': { type: 'text', value: '' }
 }
